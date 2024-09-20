a21aa0947dae9926dc669354c7692067dce502b1
jaylee <jayginwoolee98@gmail.com>
2024-09-21 02:03:55 +0900

    Add data to render on modal

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,50 @@
 <template>
+  <div class="modal" v-if="selectedStudio">
+    <div class="modal-content">
+      <h4>{{ selectedStudio.title }}</h4>
+      <img :src="selectedStudio.image" class="room-img" />
+      <p>{{ selectedStudio.content }}</p>
+      <p>{{ selectedStudio.price }}원</p>
+      <button @click="selectedStudio = null">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
 
-  <div class="black-bg" v-if="isModalOpen == true">
-    <div class="white-bg">
-      <h4>상세페이지임</h4>
-      <p>상세페이지 내용임</p>
-      <button @click="isModalOpen = false">닫기</button>
-
-    </div>
-  </div>
-
-  <div>
-    <!-- <div v-for="product in products" :key="product"> -->
-    <!-- <h4>{{ product }}</h4> -->
-    <img src="./assets/room0.jpg" class="room-img" />
-    <h4 @click="isModalOpen = true">{{ products[0] }}</h4>
-    <p>50 만원</p>
-    <button @click="reports[0]++">허위매물신고</button>
-    <span>신고 수: {{ reports[0] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room1.jpg" class="room-img" />
-    <h4 @click="isModalOpen = true">{{ products[1] }}</h4>
-    <p>60 만원</p>
-    <button @click="reports[1]++">허위매물신고</button>
-    <span>신고 수: {{ reports[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" class="room-img" />
-    <h4 @click="isModalOpen = true"> {{ products[2] }}</h4>
-    <p>70 만원</p>
-    <button @click="reports[2]++">허위매물신고</button>
-    <span>신고 수: {{ reports[2] }}</span>
+  <div v-for="(a, i) in studios" :key="i">
+    <img :src="a.image" class="room-img" />
+    <h4 @click="openModal(a)" style="cursor: pointer">{{ a.title }}</h4>
+    <p>{{ a.price }}원</p>
   </div>
 </template>
 
 <script>
+import studios from "./assets/oneroom.js";
+
 export default {
   name: "App",
   data() {
     return {
-      isModalOpen : false,
-      reports: [0, 0, 0],
+      studios: studios,
+      selectedStudio: null,
       menus: ["Home", "Products", "About"],
-      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
   },
   methods: {
-    increase() {
-      this.reports++;
+    openModal(studio) {
+      this.selectedStudio = studio;
     },
+  },
+  watch: {
+    selectedStudio(newValue) {
+      if (newValue) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = '';
+      }
+    }
   },
   components: {},
 };
@@ -66,19 +59,28 @@ div {
   box-sizing: border-box;
 }
 
-.black-bg {
+.modal {
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  padding: 20px;
+  background-color: rgba(0,0,0,0.4);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.white-bg {
-  width: 100%;
-  background: white;
-  border-radius: 8px;
+.modal-content {
+  background-color: #fefefe;
   padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 600px;
+  max-height: 80vh;
+  overflow-y: auto;
+  border-radius: 5px;
 }
 
 .room-img {
